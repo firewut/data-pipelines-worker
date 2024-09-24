dbf3b7afe2282e764e02c67153a0139f41061117
Andrey Chibisov <andrey844@gmail.com>
2024-09-24 19:34:28 +0100

    Added new block image-blur

---
+++ test/unit/block_image_blur_test.go
@@ -0,0 +1,127 @@
+package unit_test
+
+import (
+	"bytes"
+	"image"
+	"image/png"
+
+	"data-pipelines-worker/types/blocks"
+	"data-pipelines-worker/types/dataclasses"
+	"data-pipelines-worker/types/helpers"
+	"data-pipelines-worker/types/validators"
+)
+
+func (suite *UnitTestSuite) TestBlockImageBlur() {
+	block := blocks.NewBlockImageBlur()
+
+	suite.Equal("image_blur", block.GetId())
+	suite.Equal("Image Blur", block.GetName())
+	suite.Equal("Blur Image", block.GetDescription())
+	suite.NotNil(block.GetSchema())
+	suite.NotEmpty(block.GetSchemaString())
+	suite.NotEmpty(block.GetConfigSection())
+
+	blockConfig := &blocks.BlockImageBlurConfig{}
+	helpers.MapToYAMLStruct(
+		block.GetConfigSection(),
+		blockConfig,
+	)
+	suite.Equal(1.5, blockConfig.Sigma)
+}
+
+func (suite *UnitTestSuite) TestBlockImageBlurValidateSchemaOk() {
+	block := blocks.NewBlockImageBlur()
+
+	schemaPtr, schema, err := block.ValidateSchema(validators.JSONSchemaValidator{})
+	suite.Nil(err)
+	suite.NotNil(schemaPtr)
+	suite.NotNil(schema)
+}
+
+func (suite *UnitTestSuite) TestBlockImageBlurValidateSchemaFail() {
+	block := blocks.NewBlockImageBlur()
+
+	block.SchemaString = "{invalid schema"
+
+	_, _, err := block.ValidateSchema(validators.JSONSchemaValidator{})
+	suite.NotNil(err)
+}
+
+func (suite *UnitTestSuite) TestBlockImageBlurProcessIncorrectInput() {
+	// Given
+	block := blocks.NewBlockImageBlur()
+	data := &dataclasses.BlockData{
+		Id:   "image_blur",
+		Slug: "image-blur",
+		Input: map[string]interface{}{
+			"image": nil,
+		},
+	}
+
+	// When
+	result, err := block.Process(
+		suite.GetContextWithcancel(),
+		blocks.NewProcessorImageBlur(),
+		data,
+	)
+
+	// Then
+	suite.Empty(result)
+	suite.NotNil(err)
+}
+
+func (suite *UnitTestSuite) TestBlockImageBlurProcessSuccess() {
+	// Given
+	width := 300
+	height := 300
+
+	sigmas := []float64{1.0, 2.0, 3.0, 4.0}
+
+	type testCase struct {
+		sigma float64
+	}
+	testCases := []testCase{}
+	for _, s := range sigmas {
+		testCases = append(testCases, testCase{s})
+	}
+
+	images := make([]image.Image, 0)
+	for _, tc := range testCases {
+		imageBuffer := suite.GetPNGImageBuffer(width, height)
+
+		block := blocks.NewBlockImageBlur()
+		data := &dataclasses.BlockData{
+			Id:   "image_blur",
+			Slug: "image-blur",
+			Input: map[string]interface{}{
+				"image": imageBuffer.Bytes(),
+				"sigma": tc.sigma,
+			},
+		}
+
+		// When
+		result, err := block.Process(
+			suite.GetContextWithcancel(),
+			blocks.NewProcessorImageBlur(),
+			data,
+		)
+
+		// Then
+		suite.NotNil(result)
+		suite.Nil(err)
+
+		image, err := png.Decode(bytes.NewReader(result.Bytes()))
+		suite.Nil(err)
+		suite.NotNil(image)
+		suite.Equal(width, image.Bounds().Dx())
+		suite.Equal(height, image.Bounds().Dy())
+		images = append(images, image)
+	}
+
+	// Check that all images are different
+	for i := 0; i < len(images); i++ {
+		for j := i + 1; j < len(images); j++ {
+			suite.NotEqual(images[i], images[j])
+		}
+	}
+}
---
+++ types/blocks/block_image_blur.go
@@ -0,0 +1,152 @@
+package blocks
+
+import (
+	"bytes"
+	"context"
+	"image"
+	"image/png"
+
+	"github.com/disintegration/imaging"
+
+	"data-pipelines-worker/types/config"
+	"data-pipelines-worker/types/helpers"
+	"data-pipelines-worker/types/interfaces"
+)
+
+type DetectorBlockImageBlur struct {
+	BlockDetectorParent
+}
+
+func NewDetectorBlockImageBlur(
+	detectorConfig config.BlockConfigDetector,
+) *DetectorBlockImageBlur {
+	return &DetectorBlockImageBlur{
+		BlockDetectorParent: NewDetectorParent(detectorConfig),
+	}
+}
+
+func (d *DetectorBlockImageBlur) Detect() bool {
+	d.Lock()
+	defer d.Unlock()
+
+	return true
+}
+
+type ProcessorImageBlur struct {
+}
+
+func NewProcessorImageBlur() *ProcessorImageBlur {
+	return &ProcessorImageBlur{}
+}
+
+func (p *ProcessorImageBlur) Process(
+	ctx context.Context,
+	block interfaces.Block,
+	data interfaces.ProcessableBlockData,
+) (*bytes.Buffer, error) {
+	var (
+		output      *bytes.Buffer         = &bytes.Buffer{}
+		blockConfig *BlockImageBlurConfig = &BlockImageBlurConfig{}
+	)
+	_data := data.GetInputData().(map[string]interface{})
+
+	logger := config.GetLogger()
+	logger.Debugf("Starting HTTP request for block %s", data.GetSlug())
+
+	// Default value from YAML config
+	defaultBlockConfig := &BlockImageBlurConfig{}
+	helpers.MapToYAMLStruct(block.GetConfigSection(), defaultBlockConfig)
+
+	// User defined values from data
+	userBlockConfig := &BlockImageBlurConfig{}
+	helpers.MapToJSONStruct(_data, userBlockConfig)
+
+	// Merge the default and user defined maps to BlockConfig
+	helpers.MergeStructs(defaultBlockConfig, userBlockConfig, blockConfig)
+
+	imageBytes, err := helpers.GetValue[[]byte](_data, "image")
+	if err != nil {
+		return nil, err
+	}
+
+	imgBuf := bytes.NewBuffer(imageBytes)
+	img, format, err := image.Decode(imgBuf)
+	if err != nil {
+		return nil, err
+	}
+	config.GetLogger().Debugf("Image format: %s", format)
+
+	sigma, err := helpers.GetValue[float64](_data, "sigma")
+	if err != nil {
+		return nil, err
+	}
+
+	blurredImage := imaging.Blur(img, sigma)
+
+	err = png.Encode(output, blurredImage)
+	if err != nil {
+		config.GetLogger().Fatalf("Failed to encode PNG image: %v", err)
+	}
+
+	return output, nil
+}
+
+type BlockImageBlurConfig struct {
+	Sigma float64 `yaml:"sigma" json:"sigma"`
+}
+
+type BlockImageBlur struct {
+	BlockParent
+	Config *BlockImageBlurConfig
+}
+
+func NewBlockImageBlur() *BlockImageBlur {
+	_config := config.GetConfig()
+
+	block := &BlockImageBlur{
+		BlockParent: BlockParent{
+			Id:          "image_blur",
+			Name:        "Image Blur",
+			Description: "Blur Image",
+			Version:     "1",
+			SchemaString: `{
+				"type": "object",
+				"properties": {
+					"input":{
+						"type": "object",
+						"description": "Input data",
+						"properties": {
+							"image": {
+								"description": "Image to add text to",
+								"type": ["string", "object"]
+							},
+							"sigma": {
+								"description": "Blur sigma",
+								"type": "number",
+								"format": "float",
+								"default": 1.5
+							}
+						},
+						"required": ["image"]
+					},
+					"output": {
+						"description": "Blurd image",
+						"type": ["string", "null"],
+						"format": "file"
+					}
+				}
+			}`,
+			SchemaPtr: nil,
+			Schema:    nil,
+		},
+	}
+
+	if err := block.ApplySchema(block.GetSchemaString()); err != nil {
+		panic(err)
+	}
+
+	block.SetConfigSection(_config.Blocks[block.GetId()].Config)
+	block.SetProcessor(NewProcessorImageBlur())
+
+	return block
+}
